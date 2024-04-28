ad294eed6bb3816f0495a8189c2efa4d6b91bdb6
zThundy <antonioantonelli7@gmail.com>
2024-04-28 16:40:57 +0200

    Refactored event handling logic in EventsHandler class

--- classes/eventsHandler.js
+++ classes/eventsHandler.js
@@ -3,6 +3,11 @@ class EventsHandler {
     this.events = {};
   }
 
+  getEvents() {
+    // get a list of events names
+    return Object.keys(this.events);
+  }
+
   addEvent(event, req, res) {
     const userId = req.body.authenticatedUserId;
     if (!this.events[event]) {
@@ -39,9 +44,53 @@ class EventsHandler {
     });
   }
 
+  /**
+   * Used to send event data to a specific client or a list of clients
+   * 
+   * @param {String} event name of the event
+   * @param {String || Array} userId is string, then send the event to the specified user, could also be an array of user ids
+   * @param {*} data 
+   */
+  sendEventTo(event, userId, data) {
+    if (!this.events[event]) {
+      console.log(`No clients connected to event ${event} or event does not exist`);
+      return;
+    }
+
+    // if userid is array of numbers then send to all users in the array
+    if (Array.isArray(userId)) {
+      console.log(`Sending: ${jsonData} to ${event} event clients`);
+      userId.forEach(id => {
+        const jsonData = JSON.stringify(data);
+        const message = `data: ${jsonData}\n\n`;
+        const client = this.events[event].find(client => client.id === id);
+        if (client) {
+          client.res.write(message);
+        }
+      });
+    } else {
+      const jsonData = JSON.stringify(data);
+      const message = `data: ${jsonData}\n\n`;
+      console.log(`Sending: ${jsonData} to ${event} event clients`);
+      const client = this.events[event].find(client => client.id === userId);
+      if (client) {
+        client.res.write(message);
+      } else {
+        console.log(`User ${userId} not found in ${event} event clients`);
+      }
+    }
+  }
+
+  /**
+   * Used to send event data to all subscribed clients
+   * 
+   * @param {String} event name of the event
+   * @param {*} data 
+   * @returns 
+   */
   sendEvent(event, data) {
     if (!this.events[event]) {
-      console.log(`No clients connected to ${event} or event does not exist`);
+      console.log(`No clients connected to event ${event} or event does not exist`);
       return;
     }
 
