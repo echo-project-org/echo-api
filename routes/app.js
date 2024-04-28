63eb580312171e7e50ef711ffe1d266a5368d69e
zThundy <antonioantonelli7@gmail.com>
2024-04-28 16:41:01 +0200

    Refactored routes in app.js for authentication and removed commented out code

--- routes/app.js
+++ routes/app.js
@@ -1,57 +1,74 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/socket', (req, res) => {
-    con.query("SELECT lastIP FROM users WHERE id = 1", function (err, result, fields) {
-        if (err) return res.status(400).send({ error: "You messed up the request." });
+router.use((req, res, next) => {
+    const body = req.authenticator.checkToken(req, res);
+    if (!body) return res.status(401).send({ message: "You are not authorized to do this." });
+    if (body.scope !== "self") return res.status(401).send({ message: "You are not authorized to do this." });
 
-        if (result.length > 0) {
-            res.status(200).send({
-                address: result[0].lastIP,
-                port: 6981
-            });
-        } else {
-            res.status(400).send({
-                message: "Server address not found. WTF??"
-            });
-        }
-    });
+    // get user id from token
+    const uId = req.authenticator.getUserId(req.headers.authorization);
+    //add user id to request
+    req.body.authenticatedUserId = uId;
+
+    next();
 });
 
-router.get('/settings', (req, res) => {
-    con.query("SELECT sampleRate, bits, channels FROM audioSettings WHERE id = 1", function (err, result, fields) {
-        if (err) return res.status(400).send({ error: "You messed up the request." });
-
-        if (result.length > 0) {
-            res.status(200).send({
-                sampleRate: result[0].sampleRate,
-                bits: result[0].bits,
-                channels: result[0].channels
-            });
-        } else {
-            res.status(400).send({
-                message: "Audio settings not found. WTF??"
-            });
-        }
-    });
+router.get('/ping', (req, res) => {
+    res.status(200).send({ message: "pong" });
 });
 
-router.get('/version', (req, res) => {
-    con.query("SELECT id, downloadUrl FROM updaterVersions order by id desc limit 1", function (err, result, fields) {
-        if (err) return res.status(400).send({ error: "You messed up the request." });
+// router.get('/socket', (req, res) => {
+//     con.query("SELECT lastIP FROM users WHERE id = 1", function (err, result, fields) {
+//         if (err) return res.status(400).send({ error: "You messed up the request." });
 
-        if (result.length > 0) {
-            res.status(200).send({
-                version: result[0].id,
-                url: result[0].downloadUrl,
-            });
-        } else {
-            res.status(400).send({
-                message: "Server address not found. WTF??"
-            });
-        }
-    });
-});
+//         if (result.length > 0) {
+//             res.status(200).send({
+//                 address: result[0].lastIP,
+//                 port: 6981
+//             });
+//         } else {
+//             res.status(400).send({
+//                 message: "Server address not found. WTF??"
+//             });
+//         }
+//     });
+// });
+
+// router.get('/settings', (req, res) => {
+//     con.query("SELECT sampleRate, bits, channels FROM audioSettings WHERE id = 1", function (err, result, fields) {
+//         if (err) return res.status(400).send({ error: "You messed up the request." });
+
+//         if (result.length > 0) {
+//             res.status(200).send({
+//                 sampleRate: result[0].sampleRate,
+//                 bits: result[0].bits,
+//                 channels: result[0].channels
+//             });
+//         } else {
+//             res.status(400).send({
+//                 message: "Audio settings not found. WTF??"
+//             });
+//         }
+//     });
+// });
+
+// router.get('/version', (req, res) => {
+//     con.query("SELECT id, downloadUrl FROM updaterVersions order by id desc limit 1", function (err, result, fields) {
+//         if (err) return res.status(400).send({ error: "You messed up the request." });
+
+//         if (result.length > 0) {
+//             res.status(200).send({
+//                 version: result[0].id,
+//                 url: result[0].downloadUrl,
+//             });
+//         } else {
+//             res.status(400).send({
+//                 message: "Server address not found. WTF??"
+//             });
+//         }
+//     });
+// });
 
 // router.get('/ve', (req, res) => {
 //     con.query("SELECT id, downloadUrl FROM timspikVersions order by id desc limit 1", function (err, result, fields) {
