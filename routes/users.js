4ad34b2afc0304e08a8b131bf8b52e04a588f73f
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2025-03-22 16:15:20 +0100

    Started updating users route

--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,63 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+
 const {
-    fullAuthenticationMiddleware,
-    partialAuthenticationMiddleware
+    authMiddleware,
 } = require("../classes/utils");
 
-router.get("/:id", partialAuthenticationMiddleware, (req, res) => {
-    const { id } = req.params;
-    if (!id) return res.status(400).send({ message: "You messed up the request." });
+router.post("/auth", (req, res) => {
+    const { hashedIdentity } = req.body;
+    if (!hashedIdentity) return res.status(400).send({ message: "You didn't privide an identity." });
 
-    req.database.query(`
-        SELECT users.id, users.name, users.email, users.img, users.online, user_status.status, users.firstJoin, users.lastSeen
+    req.database.get("SELECT id FROM users WHERE hashedIdentity = ?", [hashedIdentity], (err, result) => {
+        if (err) return res.status(400).send({ error: "You messed up the request.", message: err });
+        if (result) {
+            req.authenticator.generateToken({ id: result.id }).then((token) => {
+                res.status(200).send({ token: token });
+            }).catch((err) => {
+                res.status(500).send({ error: "Failed to generate token." });
+            });
+        } else {
+            //add user to database
+            req.database.run("INSERT INTO users (hashedIdentity) VALUES (?)", [hashedIdentity], function (err) {
+                if (err) return res.status(400).send({ error: "You messed up the request.", message: err });
+                //get the id of the user
+                req.database.get("SELECT id FROM users WHERE hashedIdentity = ?", [hashedIdentity], (err, result) => {
+                    if (err) return res.status(400).send({ error: "You messed up the request.", message: err });
+                    if (result) {
+                        req.authenticator.generateToken({ id: result.id }).then((token) => {
+                            res.status(200).send({ token: token });
+                        }).catch((err) => {
+                            res.status(500).send({ error: "Failed to generate token." });
+                        });
+                    } else {
+                        res.status(500).send({ error: "Failed to add the user to the server" });
+                    }
+                });
+            });
+        }
+    });
+});
+
+router.get("/verifyToken", authMiddleware, (req, res) => {
+    res.status(200).send({ message: "Token is valid.", id: req.validatedId });
+});
+
+router.get("/:id", authMiddleware, (req, res) => {
+    const { id } = req.params;
+    if (!id) return res.status(400).send({ message: "Please provide an id" });
+
+    console.log("Getting user with id", id);
+    req.database.get(`
+        SELECT users.id, users.username, users.lastLogin, users.firstLogin, users.img, users.online, users.muted, users.deaf
         FROM users
-        LEFT JOIN user_status ON userId = id
         WHERE id = ?
     `, [id], (err, result, fields) => {
-        if (err) return res.status(400).send({ error: "You messed up the request." });
-        if (result.length > 0) {
-            res.status(200).send(result[0]);
+        if (err) return res.status(400).send({ error: "You messed up the request.", message: err });
+        console.log(result)
+        if (result) {
+            res.status(200).send(result);
         } else {
             res.status(404).send({ error: "User not found." });
         }
@@ -49,7 +88,7 @@ router.get("/image/:id", (req, res) => {
     // res.status(404).send("File not found");
 });
 
-router.post("/image", fullAuthenticationMiddleware, (req, res) => {
+router.post("/image", (req, res) => {
     var { id, image } = req.body;
     if (!id || !image) return res.status(400).send({ message: "You messed up the request." });
 
@@ -74,7 +113,7 @@ router.post("/image", fullAuthenticationMiddleware, (req, res) => {
             res.status(400).send({ message: "Error saving image" });
         } else {
             const imgUrl = "https://echo.kuricki.com/api/users/image/" + id + ".png";
-            req.database.query("UPDATE users SET img = ? WHERE id = ?", [imgUrl, id], (err, result, fields) => {
+            req.database.run("UPDATE users SET img = ? WHERE id = ?", [imgUrl, id], (err, result, fields) => {
                 // console.log(err);
                 if (err) return res.status(400).send({ error: "You messed up the request." });
                 res.status(200).send({ message: "Image updated!", url: imgUrl });
@@ -84,11 +123,11 @@ router.post("/image", fullAuthenticationMiddleware, (req, res) => {
     });
 });
 
-router.get("/status/:id", partialAuthenticationMiddleware, (req, res) => {
+router.get("/status/:id", (req, res) => {
     const { id } = req.query;
     if (!id) return res.status(400).send({ message: "You messed up the request." });
 
-    req.database.query("SELECT online FROM users WHERE id = ?", [id], (err, user, fields) => {
+    req.database.get("SELECT online FROM users WHERE id = ?", [id], (err, user, fields) => {
         if (err) return res.status(400).send({ error: "You messed up the request." });
         if (user.length > 0) {
             if (user[0].online === "1") {
@@ -108,34 +147,33 @@ router.get("/status/:id", partialAuthenticationMiddleware, (req, res) => {
 });
 
 // update user status
-router.post('/status', fullAuthenticationMiddleware, (req, res) => {
+router.post('/status', (req, res) => {
     let { id, status } = req.body;
     if (!id || !status) return res.status(400).send({ message: "You messed up the request." });
 
     // set online status of user to offline
-    req.database.query("UPDATE users SET online = ? WHERE id = ?", [status, id], function (err, result, fields) {
+    req.database.run("UPDATE users SET online = ? WHERE id = ?", [status, id], function (err, result, fields) {
         if (err) console.log(err);
 
         // remove user from any rooms
         if (status === "0") {
-            req.database.query("DELETE FROM room_users WHERE userId = ?", [id], function (err, result, fields) {
+            req.database.run("DELETE FROM roomUsers WHERE userId = ?", [id], function (err, result, fields) {
                 if (err) console.log(err);
                 res.status(200).send({ message: "You are now offline!" });
             });
 
         } else {
             res.status(200).send({ message: "Status updated!" });
-            req.eventsHandler.sendEvent("users", { action: "statusUpdate", data: { userId: id, status } });
         }
     });
 });
 
-router.post("/customStatus", fullAuthenticationMiddleware, (req, res) => {
+router.post("/customStatus", (req, res) => {
     let { id, status } = req.body;
     if (!id || !status) return res.status(400).send({ message: "You messed up the request." });
 
     // update user status
-    req.database.query("UPDATE user_status SET status = ? WHERE userId = ?", [status, id], (err, result, fields) => {
+    req.database.run("UPDATE user_status SET status = ? WHERE userId = ?", [status, id], (err, result, fields) => {
         if (result.warningCount === 1) {
             console.error("Query gave an error: " + result.message);
             return res.status(400).send({ error: "You can't use this value." });
@@ -145,173 +183,6 @@ router.post("/customStatus", fullAuthenticationMiddleware, (req, res) => {
         res.status(200).send({ message: "Status updated!" });
         req.eventsHandler.sendEvent("users", { action: "customStatusUpdate", data: { userId: id, status } });
     });
-});
-
-// update volume value of existing user
-router.post('/volume', partialAuthenticationMiddleware, (req, res) => {
-    const body = req.body;
-    const id = body.id;
-    const user = body.status;
-    const volume = body.room;
-
-    req.database.query("INSERT INTO userVolumes (me, otherUser, volume) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE volume = ?", [id, user, volume, volume], function (err, result, fields) {
-        if (err) return res.status(400).send({ error: "You messed up the request." });
-        res.status(200).send({ message: "Volume updated!" });
-    });
-})
-
-// get personal volume levels from user id
-router.get('/volume/:name', partialAuthenticationMiddleware, (req, res) => {
-    const { name } = req.params;
-
-    req.database.query("SELECT otherUser, volume FROM userVolumes WHERE me = ?", [name], function (err, result, fields) {
-        if (err) return res.status(400).send({ error: "You messed up the request." });
-
-        var jsonOut = [];
-        if (result.length > 0) {
-            result.map(function (volumes) {
-                jsonOut.push({
-                    "name": volumes.otherUser,
-                    "volume": volumes.volume,
-                });
-            })
-            res.status(200).send(jsonOut);
-        } else {
-            res.status(200).send(jsonOut);
-        }
-    });
-})
-
-// get volume level of specific user
-router.get('/volume/:nick1/:nick2', partialAuthenticationMiddleware, (req, res) => {
-    const { nick1, nick2 } = req.params;
-
-    req.database.query("SELECT otherUser, volume FROM userVolumes WHERE me = ? AND otherUser = ?", [nick1, nick2], function (err, result, fields) {
-        if (err) return res.status(400).send({ error: "You messed up the request." });
-
-        var jsonOut = [];
-        if (result.length > 0) {
-            result.map(function (volumes) {
-                jsonOut.push({
-                    "name": volumes.otherUser,
-                    "volume": volumes.volume,
-                });
-            })
-            res.status(200).send(jsonOut);
-        } else {
-            res.status(200).send(jsonOut);
-        }
-    });
-});
-
-// get friends of user
-router.get('/friends/:id', fullAuthenticationMiddleware, (req, res) => {
-    const { id } = req.params;
-    if (!id) return res.status(400).send({ message: "You messed up the request." });
-
-    req.database.query(`
-        SELECT
-            CASE
-                WHEN f1.id = ? AND f2.id IS NOT NULL THEN 'friend'
-                WHEN f2.id = ? AND f1.id IS NOT NULL THEN 'friend'
-                WHEN f1.id = ? AND f2.id IS NULL THEN 'sent'
-                WHEN f2.id = ? AND f1.id IS NULL THEN 'incoming'
-                ELSE 'incoming'
-            END AS relationship,
-            CASE
-                WHEN f1.id = ? THEN f1.otherId
-                ELSE f1.id
-            END AS otherUserId,
-            u.name AS otherUsername,
-            u.img AS otherUserImage,
-            CASE
-                WHEN u.online = '1' THEN us.status
-                ELSE '0'
-            END AS otherUserStatus,
-            f1.otherId AS otherId
-        FROM user_friends f1
-        LEFT JOIN user_friends f2 ON (f1.id = f2.otherId AND f1.otherId = f2.id)
-        LEFT JOIN users u ON u.id = CASE WHEN f1.id = ? THEN f1.otherId ELSE f1.id END
-        LEFT JOIN user_status us ON us.userId = CASE WHEN f1.id = ? THEN f1.otherId ELSE f1.id END
-        WHERE f1.id = ? OR f1.otherId = ?;
-    `, [id, id, id, id, id, id, id, id, id], function (err, result, fields) {
-        if (err) {
-            console.error(err);
-            return res.status(400).send({ error: "You messed up the request." });
-        }
-
-        const friendMap = {
-            friended: [],
-            sent: [],
-            incoming: [],
-        };
-        if (result.length > 0) {
-            result.map((friends) => {
-                // check if user is friended by other user
-                if (friends.relationship === "friend" && friends.otherId !== Number(id)) {
-                    friendMap.friended.push({
-                        id: friends.otherUserId,
-                        img: friends.otherUserImage,
-                        name: friends.otherUsername,
-                        status: friends.otherUserStatus,
-                    })
-                } else
-                    // check if user sent friend request
-                    if (friends.relationship === "sent") {
-                        friendMap.sent.push({
-                            id: friends.otherUserId,
-                            img: friends.otherUserImage,
-                            name: friends.otherUsername,
-                            // status: friends.otherUserStatus,
-                            status: "-1",
-                        })
-                    } else
-                        // check if user has incoming friend request
-                        if (friends.relationship === "incoming") {
-                            friendMap.incoming.push({
-                                id: friends.otherUserId,
-                                img: friends.otherUserImage,
-                                name: friends.otherUsername,
-                                // status: friends.otherUserStatus,
-                                status: "-1",
-                            })
-                        }
-            });
-            res.status(200).send(friendMap);
-        } else {
-            res.status(200).send(friendMap);
-        }
-    });
-})
-
-// operation on friend request
-router.post('/friend/request', fullAuthenticationMiddleware, (req, res) => {
-    const { id, friendId, operation } = req.body;
-    if (!id || !friendId || !operation) return res.status(400).send({ message: "You messed up the request." });
-
-    switch (operation) {
-        case "add":
-            req.database.query("INSERT INTO user_friends (id, otherId) VALUES (?, ?)", [id, friendId], function (err, result, fields) {
-                if (err) {
-                    console.error(err);
-                    return res.status(400).send({ error: "You messed up the request." });
-                }
-                res.status(200).send({ message: "Friend added!" });
-            });
-            break;
-        case "remove":
-            req.database.query("DELETE FROM user_friends WHERE id = ? AND otherId = ? || id= ? AND otherId = ?", [id, friendId, friendId, id], function (err, result, fields) {
-                if (err) {
-                    console.error(err);
-                    return res.status(400).send({ error: "You messed up the request." });
-                }
-                res.status(200).send({ message: "Friend removed!" });
-            });
-            break;
-        default:
-            res.status(404).send({ message: "Unknown operation." });
-            break;
-    }
 });
 
 module.exports = router;
